0475dd823c66bce1baec0d0541d0fdef26d7956d
tu nombre <carmencoboruizazuaga@gmail.com>
2022-05-04 14:38:10 +0200

    sustituido java

--- java.java
+++ java.java
@@ -1,33 +1,63 @@
 package busquedabinaria;
 
+
+/**
+ * 
+ * @author carmen
+ * @version 1.0 03/05/22
+ */
+
 public class busquedabinaria {
 	public static int contador;
 
-
+	
+	/**Main que genera el vector en el que posteriormente buscará un número específico y devolverá su posición.
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
+
 		int[] v = new int[40000000];
+
 		for(int j=0;j<v.length; j++) {
+
 			v[j] = j;
+
 		}
 
 		int busca = 3447;
-		System.out.print("Valor: " + buscab(v,busca) + " encontrado en el intento " + contador);
-	}
 
+		System.out.print("Valor: " + buscab(v,busca) + " encontrado en el intento " + contador);
+
+	}
 	
+	/**Busca el número dentro del vector, siempre que lo encuentra devuelve 0; si devuelve -1 es porque el número no está en el vector.
+	 * 
+	 * @param vector
+	 * @param valorabuscar
+	 * @return
+	 */
 	public static int buscab(int[] vector, int valorabuscar) {
+
 		int mitad, inferior = 0;
+
 		int superior = vector.length - 1;
+
 		contador = 0;
 
-		do{
+		do {
+
 			contador++;
+
 			mitad = (inferior + superior) >>> 1;
 
 			if(valorabuscar > vector[mitad]) {
+
 				inferior = mitad + 1;
 
 			} else {
+
 				superior = mitad - 1;
 
 			}
@@ -35,9 +65,11 @@ public class busquedabinaria {
 		} while(vector[mitad] != valorabuscar && inferior <= superior);
 
 		if(vector[mitad] == valorabuscar) {
+
 			return mitad;
 
 		} else {
+
 			return -1;
 
 		}
@@ -45,4 +77,3 @@ public class busquedabinaria {
 	}
 
 }
-
